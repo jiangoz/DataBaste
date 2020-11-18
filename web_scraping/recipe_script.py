abf0edb0e0c0d6c779d78a83d57fbb696ede2809
MMorehead <mmorehead@Margarets-MacBook-Pro.local>
2020-11-18 14:29:18 -0500

    Generate final recipes table

--- web_scraping/recipe_script.py
+++ web_scraping/recipe_script.py
@@ -1,6 +1,13 @@
 import pandas
 import requests
 
+ALL_RECIPES_URL = "https://www.allrecipes.com/recipe/"
+SERVING_STRING1 = "data-servings="
+SERVING_STRING2 = '<meta id="metaRecipeServings" itemprop="recipeYield" content="'
+INPUT_FILE = 'csv/recipes_init.csv'
+OUTPUT_PATH = 'csv/recipes_final.csv'
+CREATE = True   #represents if the servings col is being created or modified
+
 # converts a given time string into minutes
 # inputs are in the form # [m,h,d] ...
 # where m represents minutes, h represents hours, and d represents days
@@ -22,48 +29,57 @@ def convert_to_mins(time):
         i = i + 1
     return total
 
+# if in improper format, convert the time to minutes
 def convert_time(time):
     if (time == "X"):
         time_new = "NULL"
-    else:
+    elif(('m' in str(time)) or ('h' in str(time)) or ('d' in str(time))):
         time_new = convert_to_mins(time)
-    return time_new
+        return time_new
+    else:
+        return time
 
 # get the serving size for the recipe
-def get_servings(id):
-    url = "https://www.allrecipes.com/recipe/" + str(id)
-    res = requests.get(url)
-    if(res.status_code == 200):
-        data = res.text
-        pos = data.find("data-servings=")
-        if(pos == -1):
-            return "NULL"
-        pos += 14  # account for the length of "data-servings="
-        end_pos = data.find("data-recipe-id=")
-        if(end_pos == -1):
-            end_pos = pos+10
+def get_servings(row):
+    # only get the serving size if it is not already filled in
+    if CREATE or pandas.isna(row["Servings"]):
+        id = row['RecipeID']
+        url = ALL_RECIPES_URL + str(id)
+        res = requests.get(url)
+        if(res.status_code == 200):
+            data = res.text
+            pos = data.find(SERVING_STRING1)
+            if(pos != -1):
+                pos += 14  # account for the length of "data-servings="
+                end_pos = data.find("data-recipe-id=")
+                if(end_pos == -1):
+                    end_pos = pos+10
 
-        servings = data[pos:end_pos].strip()
-        print(str(id) + ": " + servings)
-        return servings
+                servings = data[pos:end_pos].strip()
+                return servings
 
-    return "NULL"
+            #different webpage format, extract servings by a different string
+            pos = data.find(SERVING_STRING2)
+            if (pos != -1):
+                pos_end = data.find('">', pos)
+                servings = data[pos + len(SERVING_STRING2): pos_end]
+                return servings
 
+    return row["Servings"]
 
-df = pandas.read_csv('csv/recipes_init.csv', sep=';')
+# if error, remove the delimiter field -> depends on input file
+df = pandas.read_csv(INPUT_FILE, delimiter=';')
 
 # remove unnecessary columns
-del df['Review Count']
-del df['Recipe Photo']
-del df['Author']
-del df['Ingredients']
-del df['Directions']
+df = df.drop(['Review Count','Recipe Photo','Author','Ingredients', 'Directions'], axis=1, errors='ignore')
+
+CREATE = not("Servings" in df.keys())
 
 # convert the prep time, cook time, and total time to the correct format
 df['Prepare Time'] = df['Prepare Time'].apply(convert_time)
 df['Cook Time'] = df['Cook Time'].apply(convert_time)
 df['Total Time'] = df['Total Time'].apply(convert_time)
-df["Servings"] = df["RecipeID"].apply(get_servings)
+df["Servings"] = df.apply(lambda row: get_servings(row), axis=1)
 
 
-df.to_csv('recipes_modified.csv')
+df.to_csv(OUTPUT_PATH)
