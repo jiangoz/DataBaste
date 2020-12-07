c85a76e48b5273bf34c0a532c7d7cf2c1de9728c
MMorehead <mmorehead@Margarets-MacBook-Pro.local>
2020-12-07 16:43:36 -0500

    Change data analysis queries

--- DataAnalysis.sql
+++ DataAnalysis.sql
@@ -1,43 +1,33 @@
 use Databaste;
 
--- 25 most common molecules in the top 100 rated recipes
+-- Get the 15 molecules with the highest average rating
 select pubchem_id, 
-	molecule_name, 
-    count(*) as 'count' 
-from recipe
-join amount using (recipe_id)
-join ingredient  using(ingredient_id)
-join composition using (ingredient_id)
-join molecule using (pubchem_id)
-where recipe_id in 
-	(select * from
-		(select recipe_id from recipe
-		join review using (recipe_id)
-		group by recipe_id
-		order by avg(rating)
-		limit 100) tmp)
-group by pubchem_id, molecule_name
-order by count desc
-limit 25;
+	molecule_name,
+    avg(rating) as 'avg' 
+from molecule
+join composition using (pubchem_id)
+join amount using (ingredient_id)
+join recipe using (recipe_id)
+join review using (recipe_id)
+group by pubchem_id
+order by avg desc
+limit 15;
 
 
--- 25 most common flavors in the top 100 rated recipes
+-- Get the flavors for the top 2 rated molecules
 select flavor_name, 
 	count(flavor_name) as 'count' 
-from recipe
-join amount using (recipe_id)
-join ingredient using (ingredient_id)
-join composition using (ingredient_id)
+from molecule 
 join property using (pubchem_id)
 join flavor using (flavor_id)
-where recipe_id in 
-	(select * from
-		(select recipe_id from recipe
-		join review using (recipe_id)
-		group by recipe_id
-		order by avg(rating)
-		limit 100) tmp)
-group by flavor_id, flavor_name
-order by count desc
-limit 25;
-
+where pubchem_id in (select * from 
+	(select pubchem_id from molecule
+	join composition using (pubchem_id)
+	join amount using (ingredient_id)
+	join recipe using (recipe_id)
+	join review using (recipe_id)
+	group by pubchem_id
+	order by avg(rating) desc
+	limit 2)tmp)
+group by flavor_name
+order by count desc, flavor_name; 
